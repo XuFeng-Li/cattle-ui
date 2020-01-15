9d396143741b58d88a65547222eca748353826f5
xuhuiyuan <xhy@asman.com.cn>
2020-01-15 17:47:16 +0800

    feature : update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from 'rollup-plugin-babel'; // 需要使用最新JS语法，babel 转码
 import commonjs from 'rollup-plugin-commonjs'; // rollup-plugin-node-resolve 插件可以解决 ES6模块的查找导入，但是npm中的大多数包都是以CommonJS模块的形式出现的，所以需要使用这个插件将CommonJS模块转换为 ES2015 供 Rollup 处理
-import external from 'rollup-plugin-peer-deps-external'; // 打包的时候排除external列表中的包，作为外部依赖
+// import external from 'rollup-plugin-peer-deps-external'; // 打包的时候排除external列表中的包，作为外部依赖
 import postcss from 'rollup-plugin-postcss'; 
 import resolve from 'rollup-plugin-node-resolve'; // 帮助 Rollup 查找外部模块，然后安装
 import json from "rollup-plugin-json";
@@ -29,7 +29,7 @@ export default {
         format: 'cjs', // 输出文件格式为CommonJS
         sourcemap: true,
       },
-      { file: mainPkg.min, format: "cjs", plugins: [terser()] },
+      // { file: mainPkg.min, format: "cjs", plugins: [terser()] }, // 非浏览器里用的UMD的包，压缩也不需要
       {
         file: mainPkg.module,
         format: 'es',
@@ -37,10 +37,9 @@ export default {
       }
     ],
     plugins: [
-      external(), // 打包的时候排除external, 并自动将库的peerDependencies配置添加到 external 配置中;
-      resolve({
-        jsnext: true, // 表示将原来的 node 模块转化成 ES6 模块
-      }),
+      // TODO: 不打UMD的包不需要这两个
+      // resolve(),
+      // commonjs(), // 将CommonJS模块转换为ES6，以便rollup打包
       postcss({
         modules: true,
         exec: true,
@@ -56,12 +55,5 @@ export default {
         exclude: 'node_modules/**', // only transpile our source code
       }),
       json(),
-      commonjs(), // 将CommonJS模块转换为ES6，以便rollup打包
     ]
   };
-
-  // {
-  //   'namedExports': {
-  //     'dist/index.js': ['__moduleExports']
-  //   }
-  // }
