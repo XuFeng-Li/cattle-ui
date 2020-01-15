0124eae4e71a162bd053ea3d32d109b90b21739c
xuhuiyuan <xhy@asman.com.cn>
2020-01-15 10:16:57 +0800

    feature : package

--- packages/panelForm/src/registryComps.js
+++ packages/panelForm/src/registryComps.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { registerFormField, connect } from "@uform/react-schema-renderer"
-import PanelSelect from '@cattle/panelSelect'
-import PanelInput from '@cattle/panelInput'
-import PanelInputNumber from '@cattle/panelInputNumber'
+import PanelSelect from 'cattle-panelSelect'
+import PanelInput from 'cattle-panelInput'
+import PanelInputNumber from 'cattle-panelInputNumber'
 
 registerFormField(
   "panelInput",
