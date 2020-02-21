eef300268ecff79263c9a9e8c565d0fc23701796
xuhuiyuan <xhy@asman.com.cn>
2020-02-21 11:28:12 +0800

    fixed : global and index

--- packages/panelInputNumber/src/index.js
+++ packages/panelInputNumber/src/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import PropTypes from 'prop-types';
 import { isStr, simplifyFileName, trim, isFn } from "cattle-util";
-import addonHoc from './common/addon';
+import addonHoc from './common/addon/index';
 import { InputNumber } from "antd";
 import styles from './index.less';
 
