fed3632855a2cc919214bded5c1696fbaa033d71
李旭峰 <lxf@asman.com.cn>
2020-12-23 10:22:32 +0800

    reset url

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Info from 'cattle-info';
-import Upload from 'cattle-upload';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Info title='标题' info={9999999999} />
-        <Upload />
+        {/*<Upload />*/}
         123123
         <img src={logo} className="App-logo" alt="logo" />
         <p>
